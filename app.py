54d00d00dcc39799f166cfde61dc13e4d2d78278
“Error-106” <2625758243@qq.com>
2022-06-19 07:55:27 +0800

    Update resource

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import ui
 import sys
 app=ui.user.QtWidgets.QApplication(sys.argv)
 userwindow=ui.user.mainwindow()
-userwindow.setWindowIcon(ui.resource.ui_rc)
+#userwindow.setWindowIcon(ui.resource.ui_rc)
 userwindow.show()
 sys.exit(app.exec())
